2b7acd7068a175a0d83fa9ff19a2c9c06f154c4a
johnmwangi <jonesmwas356@gmail.com>
2019-04-15 16:27:21 +0300

    readme modifications

--- app/main/views.py
+++ app/main/views.py
@@ -3,33 +3,33 @@ from . import main
 from flask import render_template,request,redirect,url_for
 from ..request import get_source,article_source,get_category,get_headlines
 
+#our views
 @main.route('/')
 def index():
-
-    # view root page function that returns the index page and its data
-
-    # politicts_news = get_source('politics')
-    # business_news = get_source('business')
-    # sports_news = get_source('sports')
-    # health_news = get_source('health')
-
+    '''
+    Root function returning index/home page with data
+    '''
     source= get_source()
-    return render_template('index.html',sources=source, )
-
+    headlines = get_headlines()
+    return render_template('index.html',sources=source, headlines = headlines)
 
 @main.route('/article/<id>')
 def article(id):
 
-    #title= 'article'
-
+    '''
+    View article page function that returns the various article details page and its data
+    '''
+    # title= 'Articles'
     articles = article_source(id)
-    return render_template('article.html',articles = articles,id=id)
+    return render_template('article.html',articles= articles,id=id )
 
-@main.route('/categories/<movie_name>')
-def category(new_name):
-
+@main.route('/categories/<cat_name>')
+def category(cat_name):
+    '''
+    function to return the categories.html page and its content
+    '''
     category = get_category(cat_name)
-    title = f'{new_name}'
-    new = new_name
+    title = f'{cat_name}'
+    cat = cat_name
 
-    return render_template('categories.html',title=title,category = category, new= new_name)
+    return render_template('categories.html',title = title,category = category, cat= cat_name)
